8f7ef76b76390a08085ae2494ccffde0c2f295ae
Aniket Maurya <aniketmaurya@outlook.com>
2021-11-26 11:01:51 +0530

    add tests

--- part_1/02_requests_and_routing/api.py
+++ part_1/02_requests_and_routing/api.py
@@ -1,5 +1,8 @@
 from webob import Request, Response
+from requests import Session as RequestsSession
+from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
 from parse import parse
+import inspect
 
 
 class API:
@@ -36,8 +39,17 @@ class API:
     def handle_request(self, request: Request) -> Response:
         response = Response()
         handler, path_kwargs = self.find_handler(request)
+        if inspect.isclass(handler):
+            handler = getattr(handler(), request.method.lower(), None)
+            if handler is None:
+                raise AttributeError("Method not allowed", request.method)
         if handler:
             handler(request, response, **path_kwargs)
         else:
             self.default_response(response)
         return response
+
+    def test_session(self, base_url="http://testserver"):
+        session = RequestsSession()
+        session.mount(prefix=base_url, adapter=RequestsWSGIAdapter(self))
+        return session
---
+++ part_1/02_requests_and_routing/test_bumbo.py
@@ -0,0 +1,44 @@
+import pytest
+
+
+def test_basic_route_adding(api):
+    @api.route("/home")
+    def home(req, resp):
+        resp.text = "YOLO"
+
+
+def test_route_overlap_throws_exception(api):
+    @api.route("/home")
+    def home(req, resp):
+        resp.text = "YOLO"
+
+    with pytest.raises(AssertionError):
+
+        @api.route("/home")
+        def home2(req, resp):
+            resp.text = "YOLO"
+
+
+def test_parameterized_route(api, client):
+    @api.route("/{name}")
+    def hello(req, resp, name):
+        resp.text = f"hey {name}"
+
+    assert client.get("http://testserver/matthew").text == "hey matthew"
+    assert client.get("http://testserver/ashley").text == "hey ashley"
+
+
+def test_parameterized_route(api, client):
+    @api.route("/{name}")
+    def hello(req, resp, name):
+        resp.text = f"hey {name}"
+
+    assert client.get("http://testserver/matthew").text == "hey matthew"
+    assert client.get("http://testserver/ashley").text == "hey ashley"
+
+
+def test_default_404_response(client):
+    response = client.get("http://testserver/doesnotexist")
+
+    assert response.status_code == 404
+    assert response.text == "Not found."
