db8c411cd201facb4cfb7922d8d511ab9a28b68b
Aniket Maurya <aniketmaurya@outlook.com>
2021-11-28 11:57:52 +0530

    test middleware

--- part_1/02_requests_and_routing/app.py
+++ part_1/02_requests_and_routing/app.py
@@ -1,6 +1,17 @@
 from api import API
+from middleware import Middleware
+
+
+class SimpleCustomMiddleware(Middleware):
+    def process_request(self, req):
+        print("Processing request", req.url)
+
+    def process_response(self, req, res):
+        print("Processing response", req.url)
+
 
 app = API()
+app.add_middleware(SimpleCustomMiddleware)
 
 
 @app.route("/home")
